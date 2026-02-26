32f2293ede68e5abed2a69a7590e595e1adce581
Ian Manhica <ianmanhica4@gmail.com>
2026-02-26 22:22:31 +0200

    Criação da tabela participante

---
+++ src/main/java/com/manhica/sistemadeevento/entities/Participante.java
@@ -0,0 +1,55 @@
+package com.manhica.sistemadeevento.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_participante")
+public class Participante {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String nome;
+
+    @Column(unique = true)
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Participante that = (Participante) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
